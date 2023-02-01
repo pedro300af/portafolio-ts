8a752cf375423c922be7411674ab06a6e0fea6b5
Pedro Arriaga <72060879+pedro300af@users.noreply.github.com>
2023-02-01 17:48:52 -0600

    add material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import {App} from './App'
+import theme from './theme/theme';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+   <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
